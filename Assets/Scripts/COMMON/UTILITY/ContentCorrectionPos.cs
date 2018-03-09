d079fe318a74bd426a2288a768fe1904f9c50220
SOMStudio <31519774+SOMStudio@users.noreply.github.com>
2018-03-09 22:58:51 +0200

    Content correction
    Content correction in scroll wiew element

---
+++ Assets/Scripts/COMMON/UTILITY/ContentCorrectionPos.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContentCorrectionPos : MonoBehaviour {
+
+	private Transform transformActive;
+
+	void Awake () {
+		transformActive = GetComponent<Transform> ();
+	}
+
+	void Start () {
+		if (transformActive.localPosition.y > 1) {
+			transformActive.localPosition = new Vector2 (transformActive.localPosition.x, 0);
+		}
+	}
+}
